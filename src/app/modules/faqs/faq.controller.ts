5eee754e32fbca5207dfd2057a605a9f3ec7999d
reshad21 <miakala7@gmail.com>
2025-08-17 17:48:41 +0600

    feat: implement FAQ module with CRUD operations, validation, and routing

---
+++ src/app/modules/faqs/faq.controller.ts
@@ -0,0 +1,77 @@
+import catchAsync from '../../utils/catchAsync';
+import { getSingleImageUrl } from '../../utils/getImageUrl';
+import sendResponse from '../../utils/sendResponse';
+import { FaqsServices } from './faq.service';
+
+const createFaqs = catchAsync(async (req, res) => {
+
+  const response = await FaqsServices.createFaqsIntoDB({
+    ...req.body,
+  });
+
+  sendResponse(res, {
+    statusCode: 201,
+    success: true,
+    message: 'Faqs created successfully',
+    data: response,
+  });
+});
+
+const getAllFaqs = catchAsync(async (req, res) => {
+  const { meta, data } = await FaqsServices.getAllFaqsFromDB(req.query);
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Faqs retrieved successfully',
+    meta,
+    data,
+  });
+});
+
+const getSingleFaqs = catchAsync(async (req, res) => {
+  const response = await FaqsServices.getSingleFaqsFromDB(req.params.id);
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Faqs retrieved successfully',
+    data: response,
+  });
+});
+
+const updateFaqs = catchAsync(async (req, res) => {
+  const thumbnailUrl = req.file ? getSingleImageUrl(req, req.file) : null;
+
+  const response = await FaqsServices.updateFaqsIntoDB(req.params.id, {
+    ...req.body,
+    thumbnail: thumbnailUrl,
+    tags: JSON.parse(req.body.tags || '[]'),
+  });
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Faqs updated successfully',
+    data: response,
+  });
+});
+
+const deleteFaqs = catchAsync(async (req, res) => {
+  const response = await FaqsServices.deleteFaqsFromDB(req.params.id);
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Faqs deleted successfully',
+    data: response,
+  });
+});
+
+export const FaqsController = {
+  createFaqs,
+  getAllFaqs,
+  getSingleFaqs,
+  updateFaqs,
+  deleteFaqs,
+};
